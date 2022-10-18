c599fed15a7497eec745d36ed706756b63ce7e6a
Avdeeva Elizaveta <seven258@mail.ru>
2022-10-18 18:28:51 +0300

    nested comments

--- main/models/comment/models.py
+++ main/models/comment/models.py
@@ -3,11 +3,14 @@ from main.models import Post, CustomUser
 
 
 class Comment(models.Model):
-    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, verbose_name='id пользователя')
-    post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE, verbose_name='id поста')
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, verbose_name='Пользователь')
+    post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE, verbose_name='Название поста')
     text = models.TextField("")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Изменен')
+    parent = models.ForeignKey('self', default=None, blank=True, null=True, on_delete=models.CASCADE, related_name='parent_%(class)s', verbose_name='Родительский комментарий')
+
+
 
     class Meta:
         db_table = 'comments'
--- main/views/comments.py
+++ main/views/comments.py
@@ -16,9 +16,50 @@ class CommentsView(View):
     #     return render(request, 'main/posts/detail.html', {'comment_form': CommentForm()})
     def post(self, request, *args, **kwargs):
         post = Post.objects.get(pk=kwargs['post_id'])
-        post.comments.create(text=request.POST['text'], user_id=request.user.id)
+        parent = request.POST.get('parent', None)
+        comment = post.comments.create(text=request.POST['text'], user_id=request.user.id)
+        if parent:
+            comment.parent = Comment.objects.get(id=int(parent))
+        comment.save()
         return redirect('posts_detail', post_id=post.id)
 
 
 
 
+
+
+        #
+        # parent = request.POST.get('parent', None)
+        # print(parent)
+        # form = CommentForm(request.POST)
+        # if form.is_valid():
+        #     comment = form.save(commit=False)
+        #     comment.post = post
+        #     comment.user = request.user
+        #     comment.parent = parent
+        #     comment.save()
+        #     return redirect('posts_detail', post_id=post.id)
+
+
+ # post = Post.objects.get(pk=kwargs['post_id'])
+        # post.comments.create(text=request.POST['text'], user_id=request.user.id)
+
+
+
+
+# def detail(request, slug):
+#   html = 'detail.html'
+#   post = get_object_or_404(Post, slug=slug)
+#   if request.method == "POST":
+#     parent = request.POST.get('parent', None)
+#     print(parent)
+#     form = CommentForm(request.POST)
+#     if form.is_valid():
+#       comment = form.save(commit=False)
+#       comment.post = post
+#       comment.user = request.user
+#       comment.parent = parent
+#       comment.save()
+#       return redirect('detail_man_post', slug=post.slug)
+#   else:
+#     form = CommentForm()
